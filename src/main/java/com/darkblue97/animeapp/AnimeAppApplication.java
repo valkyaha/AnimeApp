72aef5f5072334a7323053557d54112c3a8c9941
darkblue97 <darkvalhalla@protonmail.ch>
2021-09-12 10:04:56 +0200

    Initial commit

---
+++ src/main/java/com/darkblue97/animeapp/AnimeAppApplication.java
@@ -0,0 +1,13 @@
+package com.darkblue97.animeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/darkblue97/animeapp/AnimeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkblue97.animeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
